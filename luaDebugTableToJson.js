72a9d84c7725f4fc45972c51e72fd3c1aa70f5fd
thomb <tr.baxter@proton.me>
2025-01-05 15:28:54 -0500

    chore: cleanup console

--- luaDebugTableToJson.js
+++ luaDebugTableToJson.js
@@ -8,40 +8,34 @@ const luaDebugTableToJson = (luaString) => {
   // Replace `=>` with `:` for key-value pairs
   luaString = luaString.replace(/=>/g, ":");
 
-  const initLise = luaString.split('\n');
-  const cleaned = initLise.map((line) => { 
+  const luaLines = luaString.split('\n');
+  const cleaned = luaLines.map((line) => { 
     return line.trim().replace(/^\[([^\]]+)\]/g, '"$1"');;
   })
   luaString = cleaned.join('\n')
-
-  console.log('luaString',luaString);
-  // Normalize Lua table keys (e.g., [key] => key:)
-  /*
-  luaString = luaString.replace(/^\[([^\]]+)\]/g, '"$1"');
-  */
 
   luaString = luaString.replace(/,(\s*[}\]])/g, "$1");
   
 
   const data = luaString.trim();
-    const lines = data.split('\n');
-    const updatedLines = lines.map((line, index) => {
-      const trimmedLine = line.trim();
+  const lines = data.split('\n');
+  const updatedLines = lines.map((line, index) => {
+    const trimmedLine = line.trim();
 
-      const nextLine = lines[index + 1] || '';
-      const nextLineTrimmed = nextLine.trim();
+    const nextLine = lines[index + 1] || '';
+    const nextLineTrimmed = nextLine.trim();
 
-      if (
-        trimmedLine &&
-        !trimmedLine.endsWith(',') &&
-        !trimmedLine.endsWith('{') &&
-        !nextLineTrimmed.startsWith('}')
-      ) {
-        return line + ','; 
-      }
+    if (
+      trimmedLine &&
+      !trimmedLine.endsWith(',') &&
+      !trimmedLine.endsWith('{') &&
+      !nextLineTrimmed.startsWith('}')
+    ) {
+      return line + ','; 
+    }
 
-      return line; 
-    });
+    return line; 
+  });
 
   let output = updatedLines.join('\n');
   output = output.substring(0, output.length - 1);
